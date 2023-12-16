5e4a33e378220d9f00cc5012e7665b9211b81641
Phurit Murin <phutit.any@gmail.com>
2023-12-17 00:26:26 +0700

    update sidebar width

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -11,7 +11,7 @@ export const SideBar = (props: Props) => {
                 <div className="fixed z-50 inset-0 overflow-y-auto md:hidden" aria-modal="true">
                     <div className="fixed inset-0 bg-black/20 backdrop-blur-sm"
                          aria-hidden="true"></div>
-                    <div className="relative bg-[#F4D03F] w-80 max-w-[calc(100%-3rem)] p-6 h-screen">
+                    <div className="relative bg-[#F4D03F] w-60 max-w-[calc(100%-3rem)] p-6 h-screen">
                         <button type="button" onClick={props.onClick}
                                 className="absolute z-10 top-5 right-5 w-8 h-8 flex items-center justify-center text-white hover:text-[#8F7000]"
                         ><svg viewBox="0 0 10 10" className="w-2.5 h-2.5 overflow-visible">
